ab250a22e6d1b128d0a5ecd97f925f064ae38daf
zhangjiji1989 <752257286@qq.com>
2023-12-12 14:27:24 +0800

    添加：字符串工具类

---
+++ src/main/java/utils/StringUtils.java
@@ -0,0 +1,30 @@
+package utils;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * 字符串工具类
+ */
+public class StringUtils {
+
+    /**
+     * 判断字符串是否包含中文字符
+     * @param content 字符串
+     * @return 是否包含中文字符
+     */
+    public static boolean checkChineseCharacter(String content){
+        if (null != content && !content.isEmpty()) {
+            String regEx = "[一-龥]";
+            Pattern p = Pattern.compile(regEx);
+            Matcher m = p.matcher(content);
+            if (m.find()) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+        return false;
+    }
+
+}
