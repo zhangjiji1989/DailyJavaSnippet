a5091826a42bc7261e59593cc92e336cf8853b3a
zhangjiji1989 <752257286@qq.com>
2023-12-12 13:44:27 +0800

    添加：日期处理方法

--- src/main/java/utils/DateUtils.java
+++ src/main/java/utils/DateUtils.java
@@ -1,8 +1,64 @@
 package utils;
 
+import cn.hutool.core.date.DateUtil;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 /**
  * 日期工具类
  */
 public class DateUtils {
+
+    /**
+     * 将日期添加指定天数
+     * @param date
+     * @param amount
+     * @return
+     */
+    public static Date addDay(Date date, int amount) {
+        Calendar c = Calendar.getInstance();
+        c.setTime(date);
+        c.add(Calendar.DAY_OF_MONTH, amount);
+        return c.getTime();
+    }
+
+    /**
+     * 获取指定日期的开始时间
+     * @param date
+     * @return
+     */
+    public static Date getDateStart(Date date) {
+        if(date==null) {
+            return null;
+        }
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        try {
+            date= sdf.parse(DateUtil.format(date, "yyyy-MM-dd")+" 00:00:00");
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return date;
+    }
+
+    /**
+     *  获取指定日期的结束时间
+     * @param date
+     * @return
+     */
+    public static Date getDateEnd(Date date) {
+        if(date==null) {
+            return null;
+        }
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        try {
+            date= sdf.parse(DateUtil.format(date, "yyyy-MM-dd") +" 23:59:59");
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return date;
+    }
 
 }
